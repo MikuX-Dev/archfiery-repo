8ebad9c4a115117c8ab0e47fe2c439e02a2ba8b7
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-30 18:00:42 +0530

    done

--- x86_64/update-db.sh
+++ x86_64/update-db.sh
@@ -24,7 +24,7 @@ echo "###################################"
 ## -s: signs the packages
 ## -n: only add new packages not already in database
 ## -R: remove old package files when updating their entry
-repo-add -s -R archfiery.db.tar.gz
+repo-add -n archfiery.db.tar.gz ./*.tar.*
 
 # Removing the symlinks.
 # rm archfiery.db
